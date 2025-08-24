f20c462781a7f2d3d26ab8e3eb2eca12ed4c3ad0
Joe Rice <joerice123455t@gmail.com>
2025-08-24 01:04:50 +0100

    shit ton of fixes

--- Assets/Scripts/Assembly-CSharp/DownloadObb.cs
+++ Assets/Scripts/Assembly-CSharp/DownloadObb.cs
@@ -3,48 +3,59 @@ using UnityEngine;
 
 public class DownloadObb : MonoBehaviour
 {
-	private string _mainPath;
+    private string _mainPath;
 
-	private string _expPath;
+    private string _expPath;
 
-	private void Start()
-	{
-		if (!GooglePlayDownloader.RunningOnAndroid())
-		{
-			Application.LoadLevel("Bootloader");
-			return;
-		}
-		_expPath = GooglePlayDownloader.GetExpansionFilePath();
-		if (_expPath == null)
-		{
-			Debug.Log("External storage is not available!");
-			return;
-		}
-		_mainPath = GooglePlayDownloader.GetMainOBBPath(_expPath);
-		if (_mainPath == null)
-		{
-			GooglePlayDownloader.FetchOBB();
-		}
-		StartCoroutine(CoroutineLoadLevel());
-	}
+    private void Start()
+    {
+#if UNITY_ANDROID
+        if (!GooglePlayDownloader.RunningOnAndroid())
+        {
+            Debug.Log("Not running on Android, loading Bootloader.");
+            Application.LoadLevel("Bootloader");
+            return;
+        }
 
-	protected IEnumerator CoroutineLoadLevel()
-	{
-		while (string.IsNullOrEmpty(_mainPath))
-		{
-			_mainPath = GooglePlayDownloader.GetMainOBBPath(_expPath);
-			yield return new WaitForSeconds(2f);
-		}
-		Debug.Log("Main Path is: " + _mainPath);
-		WWW loading = WWW.LoadFromCacheOrDownload("file://" + _mainPath, 0);
-		yield return loading;
-		if (loading.error != null)
-		{
-			Debug.LogError("WWW Error: " + loading.error);
-		}
-		else
-		{
-			Application.LoadLevel("Bootloader");
-		}
-	}
+        _expPath = GooglePlayDownloader.GetExpansionFilePath();
+        if (_expPath == null)
+        {
+            Debug.Log("External storage is not available!");
+            Application.LoadLevel("Bootloader");
+            return;
+        }
+
+        _mainPath = GooglePlayDownloader.GetMainOBBPath(_expPath);
+        if (_mainPath == null)
+        {
+            GooglePlayDownloader.FetchOBB();
+        }
+
+        StartCoroutine(CoroutineLoadLevel());
+#else
+
+
+        Application.LoadLevel("Bootloader");
+#endif
+    }
+
+    protected IEnumerator CoroutineLoadLevel()
+    {
+        while (string.IsNullOrEmpty(_mainPath))
+        {
+            _mainPath = GooglePlayDownloader.GetMainOBBPath(_expPath);
+            yield return new WaitForSeconds(2f);
+        }
+        Debug.Log("Main Path is: " + _mainPath);
+        WWW loading = WWW.LoadFromCacheOrDownload("file://" + _mainPath, 0);
+        yield return loading;
+        if (loading.error != null)
+        {
+            Debug.LogError("WWW Error: " + loading.error);
+        }
+        else
+        {
+            Application.LoadLevel("Bootloader");
+        }
+    }
 }
